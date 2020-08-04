9aa12b8c0d945dad6551fe961e5337c4486d0a7e
Pedro Kruszynski <pedro.henrique@saudegarantida.com>
2020-08-04 14:07:35 -0300

    first commit

---
+++ Primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+    selector: 'meu-primeiro-component',
+    template: `
+        <p> Meu primeiro component com Angular></p>
+    `
+
+})
+export class MeuPrimeiroComponent {
+
+}
